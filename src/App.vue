c11b3063021184bd0ba9b1f8bcaa4cd8fa0c31ff
mediyed <zirimouhamed@gmail.com>
2022-06-30 13:26:48 +0200

    message

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+
+  Headermounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
+  },
+};
+</script> 
 
 <style>
 #app {
@@ -21,6 +22,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+.registre input,
+.login input {
+  width: 300px;
+  height: 40px;
+  padding: 20px;
+  display: block;
+  margin-bottom: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.registre button,
+.login button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
 }
 </style>
